3a770111d48f9afd12eb81823e7f655332e3189b
conniekang <hqk1121@gmail.com>
2021-09-21 20:44:50 -0400

    commit tree 52bfb3bf6e819e9b440f34897eb566ffad9c4ae8

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(arr, s) {
+    return arr.filter(element=>element.toLowerCase()===s.toLowerCase())
+}
+
+function fuzzyMatch(arr, s) {
+    var l = s.length
+    return arr.filter(element=>element.slice(0,l)===s)
+}
+
+function matchName(arr, s) {
+    return arr.filter(element=>element.name===s)
+}
